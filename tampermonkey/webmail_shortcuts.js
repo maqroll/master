f817fead6941a7bfedb72c4e31d3dc0d50e38e29
maqroll <loteroc@gmail.com>
2013-09-14 15:46:30 +0200

    prev/next msg, prev/next page

--- tampermonkey/webmail_shortcuts.js
+++ tampermonkey/webmail_shortcuts.js
@@ -3,7 +3,7 @@
 // @namespace      http://www.example.com/jQueryPlay/
 // @description    Shortcuts for OWA
 // @include        https://webmail.indra.es/*
-// @require       http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.js
 // @require       http://www.openjs.com/scripts/events/keyboard_shortcuts/shortcut.js
 // ==/UserScript==
 
@@ -41,6 +41,66 @@ $(document).ready(function() {
     // Cerrar
     shortcut.add("Esc",function() {
         $("#lnkHdrclose").click();
+    });
+
+    // Next message
+    shortcut.add("Shift+Down",function() {
+        var checkSelected = $(":checkbox:focus");
+        if (checkSelected.length) {
+            // si hay alguno seleccionado....pasamos al siguiente
+            var tr = checkSelected.closest("tr");
+            if (tr.length) {
+                var nextRow = tr.next();
+                if (!$(":checkbox",nextRow).length) {
+                    nextRow = nextRow.next();
+                }
+                if (nextRow.length) {
+                    var checkbox = $(":checkbox",nextRow[0]);
+                    if (checkbox.length) {
+                        checkbox.focus();
+                        return;
+                    }
+                }
+            }
+        }
+        
+        // si no hay ninguno seleccionado o es el último
+        $(":checkbox:first").focus();
+    });
+
+    // Prev message
+    shortcut.add("Shift+Up",function() {
+        var checkSelected = $(":checkbox:focus");
+        if (checkSelected.length) {
+            // si hay alguno seleccionado....pasamos al siguiente
+            var tr = checkSelected.closest("tr");
+            if (tr.length) {
+                var prevRow = tr.prev();
+                if (!$(":checkbox",prevRow).length) {
+                    prevRow = prevRow.prev();
+                }
+                if (prevRow.length) {
+                    var checkbox = $(":checkbox",prevRow[0]);
+                    if (checkbox.length) {
+                        checkbox.focus();
+                        return;
+                    }
+                }
+            }
+        }
+        
+        // si no hay ninguno seleccionado o es el último
+        $(":checkbox:last").focus();
+    });
+
+    // Next Page
+    shortcut.add("Shift+Right",function() {
+        $("#lnkNxtPgHdr").click();
+    });
+
+    // Prev Page
+    shortcut.add("Shift+Left",function() {
+        $("#lnkPrvPgHdr").click();
     });
     
 });
