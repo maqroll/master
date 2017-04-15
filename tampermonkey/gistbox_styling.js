d447d403951fb23a8c67d8ddc7503df392e9d3d8
luis <loteroc@indra.es>
2017-04-15 09:14:39 +0200

    ! added font to gistbox styling

--- tampermonkey/gistbox_styling.js
+++ tampermonkey/gistbox_styling.js
@@ -6,8 +6,19 @@
 // @author       You
 // @match        https://app.gistboxapp.com/
 // @grant        none
+// @resource customFont http://fonts.googleapis.com/css?family=Vollkorn
 // @run-at      context-menu
 // ==/UserScript==
+
+function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
 
 (function() {
     'use strict';
@@ -16,4 +27,5 @@
     document.getElementsByClassName('content-view-container')[0].style['padding-left']="0px";
     document.body.style.background="none";
 
+    addGlobalStyle("body{font:normal 100% Vollkorn;}");
 })();
