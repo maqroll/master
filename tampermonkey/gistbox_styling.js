a5935a956ec6f6ab2c08823ca88685b2ce92125b
maqroll <loteroc@gmail.com>
2017-04-22 14:03:41 +0200

    Update gistbox_styling.js

--- tampermonkey/gistbox_styling.js
+++ tampermonkey/gistbox_styling.js
@@ -7,8 +7,9 @@
 // @match        https://app.gistboxapp.com/
 // @grant        none
 // @resource customFont http://fonts.googleapis.com/css?family=Old+Standard+TT
-// @run-at      context-menu
 // ==/UserScript==
+
+var interval = 0;
 
 function addGlobalStyle(css) {
     var head, style;
@@ -20,16 +21,26 @@ function addGlobalStyle(css) {
     head.appendChild(style);
 }
 
-(function() {
+function main() {
     'use strict';
 
-    document.getElementsByClassName('sidebar-view-container')[0].style.display="none";
-    document.getElementsByClassName('content-view-container')[0].style['padding-left']="0px";
-    document.body.style.background="none";
+    if (
+        (document.getElementsByClassName('sidebar-view-container').length > 0) &&
+        (document.getElementsByClassName('content-view-container').length > 0) &&
+        (document.getElementsByClassName('icon-question-sign').length > 0) &&
+        (document.getElementsByClassName('icon-cut').length > 0))
+    {
+        document.getElementsByClassName('sidebar-view-container')[0].style.height="0px";
+        document.getElementsByClassName('content-view-container')[0].style['padding-left']="0px";
+        document.body.style.background="none";
 
-    addGlobalStyle("body{font:normal 120% 'Old Standard TT', serif;;}");
-    document.getElementsByClassName('icon-question-sign')[0].style.display="none";
-    document.getElementsByClassName('icon-cut')[0].style.display="none";
-    document.getElementById('ace_editor').innerHTML= document.getElementById('ace_editor').innerHTML.replace('.ace_editor {position: relative;','.ace_editor {position: relative;height: 500px !important;');
-})();
+        addGlobalStyle("body{font:normal 120% 'Old Standard TT', serif;;}");
 
+        document.getElementsByClassName('icon-question-sign')[0].style.display="none";
+        document.getElementsByClassName('icon-cut')[0].style.display="none";
+        document.getElementById('ace_editor').innerHTML= document.getElementById('ace_editor').innerHTML.replace('.ace_editor {position: relative;','.ace_editor {position: relative;height: 500px !important;');
+        clearInterval(interval);
+    }
+}
+
+interval = setInterval(main, 5000);
