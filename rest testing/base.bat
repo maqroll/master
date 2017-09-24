5876fdbb36484a9c6fcfe3004a205fc2ec58543e
maqroll <loteroc@gmail.com>
2017-09-24 20:34:01 +0200

    Create base.bat

---
+++ rest testing/base.bat
@@ -0,0 +1,5 @@
+@set output=%date%.%time%.output
+@set output=%output:/=.%
+@set output=%output::=.%
+@curl --trace-ascii traza.log -w "@curl-format.txt" -L -i -k -X %1 -sS %2 %3 %4 %5 %6 -o %output%
+pause
