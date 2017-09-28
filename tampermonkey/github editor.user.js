529707857c10174eba2681c090cfa4baeabca707
maqroll <loteroc@gmail.com>
2017-09-28 19:34:29 +0200

    Update github editor.user.js

--- tampermonkey/github editor.user.js
+++ tampermonkey/github editor.user.js
@@ -5,11 +5,26 @@
 // @include   https://gist.github.com/*
 // @version   1
 // @grant     none
+// @require   http://www.openjs.com/scripts/events/keyboard_shortcuts/shortcut.js 
 // ==/UserScript==
 
 
-document.onreadystatechange = function () {
-    if (document.readyState == "complete") {
-        document.getElementsByClassName('CodeMirror')[0].style.height='auto';
-    }
-}
+$(document).ready(function(){
+    $('div.CodeMirror').height('auto');
+
+    shortcut.add("Ctrl+E",function() {
+        var editBtn = document.querySelector('a[aria-label="Edit this Gist"]');
+        editBtn.click();
+    });
+
+    shortcut.add("Esc",function() {
+        var cancelBtn = document.querySelector('a.btn-danger');
+        cancelBtn.click();
+    });
+
+    shortcut.add("Ctrl+S",function() {
+        var saveBtn = document.querySelector('form.js-blob-form');
+        saveBtn.submit();
+    });
+    
+});
