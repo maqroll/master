7de7d6c5dbb9a32c1952925e18f1dc34b62ae976
maqroll <loteroc@gmail.com>
2017-12-16 12:22:15 +0100

    Update base.bat

--- rest testing/base.bat
+++ rest testing/base.bat
@@ -1,7 +1,7 @@
 @rem $1=method (POST,GET)
 @rem $2-$9=any parameters
-@set output=%date%.%time%.output
+@set output=%date%.%time%
 @set output=%output:/=.%
 @set output=%output::=.%
-@curl --trace-ascii traza.log -w "@curl-format.txt" -L -i -k -X %1 -sS %2 %3 %4 %5 %6 %7 %8 %9 -o %output%
+@curl --trace-ascii traza.log -w "@curl-format.txt" -L -k -X %1 -sS %2 %3 %4 %5 %6 %7 %8 %9 -o %output%.data -D %output%.header
 pause
