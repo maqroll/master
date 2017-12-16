62e7018a77bf8518a5f2ab50e980e146f62f7351
maqroll <loteroc@gmail.com>
2017-12-16 20:27:37 +0100

    Update get.bat

--- bat/get.bat
+++ bat/get.bat
@@ -1,12 +1,16 @@
 @echo off
-set DOWNLOADDIR=
 setlocal enabledelayedexpansion
+setlocal enableextensions
+set DOWNLOADDIR=
 cd %DOWNLOADDIR%
 set result=1
+set MAX_OVERALL=0
+IF DEFINED MAX_DOWNLOAD (set MAX_OVERALL=%MAX_DOWNLOAD%)
 FOR %%f IN (*.torrent) DO (
     for /f "tokens=*" %%i in (%%f) do (
         set result=1
-        aria2c --dht-entry-point=dht.transmissionbt.com:6881 --bt-max-peers=10 --summary-interval=600 --min-split-size=1M --listen-port=1024-1025 --enable-dht --dht-listen-port 1024-1025 --enable-color=false --seed-time=0 %%i && set result=0
+	title "%%i"
+        aria2c --dht-entry-point=dht.transmissionbt.com:6881 --max-overall-download-limit=%MAX_OVERALL% --summary-interval=600 --min-split-size=1M --listen-port=1024-1025 --enable-dht --dht-listen-port 1024-1025 --enable-color=false --seed-time=0 %%i && set result=0
     )
 
     if !result! EQU 0 (
