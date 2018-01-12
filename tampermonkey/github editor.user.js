2e1d317e7402c0d901521ad06cb9e88a189431f1
maqroll <loteroc@gmail.com>
2018-01-12 20:50:05 +0100

    Update github editor.user.js

--- tampermonkey/github editor.user.js
+++ tampermonkey/github editor.user.js
@@ -12,13 +12,12 @@ document.onreadystatechange = function () {
     if (document.readyState == "complete") {
         if (document.querySelector('div.CodeMirror')) {
             document.querySelector('div.CodeMirror').style.height='auto';
+            document.querySelector('div.CodeMirror').CodeMirror.focus();
         }
 
         if (document.querySelector('div.CodeMirror-lines')) {
             document.querySelector('div.CodeMirror-lines').style.fontSize='15px';
-            document.querySelector('div.CodeMirror-lines').focus();
         }
-
 
         shortcut.add("Ctrl+O",function() {
             var editBtn = document.querySelector('a[aria-label="Edit this Gist"]');
