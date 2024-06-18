0c0ef078d6837a32fa762bf8474cc10d268f9583
maqroll <loteroc@gmail.com>
2024-06-18 19:35:37 +0200

    cargo fmt

--- ws/src/main.rs
+++ ws/src/main.rs
@@ -1,19 +1,13 @@
-use std::env;
-use reqwest::{Client};
-use reqwest_websocket::{Message, RequestBuilderExt, Error};
 use futures_util::sink::SinkExt;
-use futures_util::{TryStreamExt};
+use futures_util::TryStreamExt;
 use log::*;
+use reqwest::Client;
+use reqwest_websocket::{Error, Message, RequestBuilderExt};
+use std::env;
 use std::io::Write;
 
-
-async fn app(url: String, req: String) -> Result<(), Error> {
-    let response = Client::default()
-        .get(url)
-        .upgrade()
-        .send()
-        .await?;
-
+async fn app(url: &String, req: &String) -> Result<(), Error> {
+    let response = Client::default().get(url).upgrade().send().await?;
 
     // turn the response into a websocket stream
     let mut websocket = response.into_websocket().await?;
@@ -26,7 +20,7 @@ async fn app(url: String, req: String) -> Result<(), Error> {
         match message {
             Message::Text(text) => {
                 println!("{text}")
-            },
+            }
             _ => {}
         }
     }
@@ -38,15 +32,17 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let start = std::time::Instant::now();
-    env_logger::Builder::from_default_env().format(move |buf, rec| {
-        let t = start.elapsed().as_secs_f32();
-        writeln!(buf, "{:.03} [{}] - {}", t, rec.level(),rec.args())
-    }).init();
+    env_logger::Builder::from_default_env()
+        .format(move |buf, rec| {
+            let t = start.elapsed().as_secs_f32();
+            writeln!(buf, "{:.03} [{}] - {}", t, rec.level(), rec.args())
+        })
+        .init();
 
     let rt = tokio::runtime::Runtime::new().unwrap();
 
-    match rt.block_on(app(args[1].clone(), args[2].clone())) {
+    match rt.block_on(app(&args[1], &args[2])) {
         Ok(_) => info!("Done"),
         Err(e) => error!("An error ocurred: {:?}", e),
-    };}
-
+    };
+}
